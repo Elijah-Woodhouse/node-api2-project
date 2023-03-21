19a513048e105b7101a74e0d019447b95aa6170b
Elijah Woodhouse <elijahwoodhouse95@gmail.com>
2023-03-21 11:04:44 -0600

    tests 1-14 passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -94,4 +94,22 @@ router.get("/", (req, res) => {
         }
     })
 
+    router.delete("/:id", async (req, res) => {
+        try {
+            const post = await Post.findById(req.params.id)
+            if(!post) {
+               res.status(404).json({
+                    message: "does not exist"
+               }) 
+            } else {
+                await Post.remove(req.params.id)
+                res.json(post)
+            }
+        } catch {
+            res.status(500).json({
+                message: "The post could not be removed"
+            })
+        }
+    })
+
 module.exports = router;
