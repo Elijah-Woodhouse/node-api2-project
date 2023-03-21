13b18702711945bbac17474cb060a32461971436
Elijah Woodhouse <elijahwoodhouse95@gmail.com>
2023-03-21 09:28:53 -0600

    tests 1-7 are up and running.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,59 @@
 // implement your posts router here
+
+const express = require("express");
+
+const Post = require("./posts-model.js");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    Post.find()
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: "Error retrieving posts",
+            });
+        });
+    });
+
+    router.get("/:id", (req, res) => {
+        Post.findById(req.params.id)
+            .then(post => {
+                if (post) {
+                    res.status(200).json(post);
+                } else {
+                    res.status(404).json({ message: "Does not exist"})
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(500).json({
+                    message: "Error retrieving the adopter",
+                });
+            });
+    });
+
+    router.post("/", async (req, res) => {
+        const { title, contents } = req.body;
+        if (!title || !contents){
+            res.status(400).json({
+                message: "Error adding post. Provide title and content."
+            })
+        } else {
+            Post.insert({title, contents})
+                .then(({ id }) => {
+                    return Post.findById(id)
+                })
+                .then(post => {
+                    res.status(201).json(post)
+                })
+                .catch(err => {
+                    res.status(500).json(err)
+                })
+        }
+    })
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,19 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express");
+
+const postsRouter = require("./posts/posts-router.js");
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/posts', postsRouter);
+
+server.get("/", (req, res) => {
+    res.send(`
+    <h2>Shelter API</h2>
+    <p>Welcome to the Shelter API</p>
+    `)
+})
+
+module.exports = server;
